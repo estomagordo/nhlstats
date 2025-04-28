134ad5ae001143d4cbdc8341b542a7369d92ff41
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2025-04-28 08:38:49 +0200

    prettier stat printing

--- process.py
+++ process.py
@@ -40,7 +40,28 @@ def process_file(file_name):
             result = 1 if home == away else 0 if home else 2
             return (True, result, lead_minute)
         return (False, -1, -1)
+    
 
+def percentage(x):
+    return f'{int(round(x, 2)*100)}%'
+
+
+def print_minute(minute):
+    total = sum(minute)
+    return f'Wins: {minute[0]} ({percentage(minute[0]/total)}). Draws: {minute[1]} ({percentage(minute[1]/total)}). Losses: {minute[2]} ({percentage(minute[2]/total)}). '
+
+
+def print_stats(minutes):
+    grand_total = sum(sum(minute) for minute in minutes)
+
+    for i, minute in enumerate(minutes):
+        print(f'Minute {i+1}: {print_minute(minute)} ({percentage(sum(minute)/grand_total)})')
+
+    wins = sum(minute[0] for minute in minutes)
+    draws = sum(minute[1] for minute in minutes)
+    losses = sum(minute[2] for minute in minutes)
+
+    print(f'Total: {print_minute([wins, draws, losses])}')
 
 def main():
     minutes = [[0, 0, 0] for _ in range(20)]
@@ -58,10 +79,7 @@ def main():
                 if succcess:
                     minutes[lead_minute][result] += 1
 
-    for i, minute in enumerate(minutes):
-        print(i, minute)
-
-    print('total', [sum(minute[i] for minute in minutes) for i in range(3)])
+    print_stats(minutes)
 
 
 if __name__ == '__main__':
